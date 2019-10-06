39fbc257efce4aae004d9f9039162531e6a86a9f
Erin <erinedawson@gmail.com>
2019-10-06 15:32:08 -0600

    add and link stylesheet

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Engrain Dev Test</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
 
