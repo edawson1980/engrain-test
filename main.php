9f600493fe8953944116f489caa4901aa25d2fc2
Erin <erinedawson@gmail.com>
2019-10-06 15:59:30 -0600

    curl init, add line to catch errors

--- main.php
+++ main.php
@@ -1,3 +1,13 @@
 <?php
 echo "hello, worllld";
+
+$curl = curl_init('https://api.sightmap.com/v1/assets/1273/multifamily/units?per-page=100');
+$resp = curl_exec($curl);
+
+//catch errors
+if (!curl_exec($curl)) {
+    die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
+}
+
+
  ?>
