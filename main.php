b9d8a640b353bf40d3d64e684b93b96bd1c99685
Erin <erinedawson@gmail.com>
2019-10-06 20:01:45 -0600

    add new curl_init()

--- main.php
+++ main.php
@@ -1,16 +1,61 @@
 <?php
 echo "hello, worllld";
 
-$curl = curl_init('https://api.sightmap.com/v1/assets/1273/multifamily/units?per-page=100?api_key=7d64ca3869544c469c3e7a586921ba37');
-// curl_setopt($ch, CURLOPT_HEADER, true, array(
-    // 'API-Key': '7d64ca3869544c469c3e7a586921ba37'
-// ));
-$resp = curl_exec($curl);
-print_r($resp);
+// $headers = array();
+// $headers[] = 'API-Key: 7d64ca3869544c469c3e7a586921ba37';
+
+// $curl = curl_init('https://api.sightmap.com/v1/assets/1273/multifamily/units?per-page=100');
+// // curl_setopt($curl, CURLOPT_URL, $url);
+// curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+//      'APIKEY: 7d64ca3869544c469c3e7a586921ba37',
+//      'Content-Type: application/json',
+//   ));
+//   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+//   curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+// $resp = curl_exec($curl);
+// $response = array();
+// $response = json_decode($resp);
+// echo print_r($response);
+// echo '<h2>' .'Property info: '. $response . '</h2>';
+
+
+
+
+
 
 //catch errors
-if (!curl_exec($curl)) {
-    die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
+// if (!curl_exec($curl)) {
+//     die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
+// }
+
+
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://api.sightmap.com/v1/assets/1273/multifamily/units?per-page=100",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => array(
+		"API-Key: 7d64ca3869544c469c3e7a586921ba37"
+	),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
 }
 
 
